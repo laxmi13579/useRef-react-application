aa4b57a5ce2d6a7a591c750baccf931dd458a08d
laxmi13579 <laxmi.yadav370515@gmail.com>
2022-06-13 22:12:11 +0545

    focus input using useRef

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 function App() {
   const [name, setName] = useState('');
   //by default it contain current object to 1, eg. {current: 1}
-  const countRender = useRef(1);
-  useEffect(()=>{
-    countRender.current = countRender.current + 1;
-  })
+  const  inputRef = useRef(1);
+  
+  function focus() {
+    inputRef.current.focus()
+  }
+
   return (
     <>
     <h1>Welcome!!</h1>
-    <input  value={name} onChange={(e)=>setName(e.target.value)}/>
+    <input ref={inputRef} value={name} onChange={(e)=>setName(e.target.value)}/>
     <p>my name is {name}</p>
-    <p>render count {countRender.current}</p>
+    <button onClick={focus} >Focus</button>
     </>
   );
 }
